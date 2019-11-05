e8f0bc705e20f0a9ed4e3ecee8a4092c020cd84a
Matt Lehner <matt.lehner@coachmeplus.com>
2019-11-05 13:35:44 -0500

    remove classes unsupported in php7

--- test/functional/cacheTest.php
+++ test/functional/cacheTest.php
@@ -503,11 +503,3 @@ $b->setHttpHeader('If-Modified-Since', sfWebResponse::getDate(sfConfig::get('LAS
 
 // test with sfFileCache class (default)
 $b->launch();
-
-// test with sfSQLiteCache class
-if (extension_loaded('SQLite') || extension_loaded('pdo_SQLite')) 
-{
-  sfConfig::set('sf_factory_view_cache', 'sfSQLiteCache');
-  sfConfig::set('sf_factory_view_cache_parameters', array('database' => sfConfig::get('sf_template_cache_dir').DIRECTORY_SEPARATOR.'cache.db'));
-  $b->launch();
-}
