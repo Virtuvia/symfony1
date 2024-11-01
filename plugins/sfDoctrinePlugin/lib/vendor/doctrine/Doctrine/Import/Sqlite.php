3396e33d261510face3a95ac761ef22e4db78ba8
Matt Lehner <matt.lehner@coachmeplus.com>
2024-11-01 00:08:36 -0400

    fix reference to $conn property for fetchAll

--- plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Import/Sqlite.php
+++ plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Import/Sqlite.php
@@ -176,7 +176,7 @@ class Doctrine_Import_Sqlite extends Doctrine_Import
     public function listTableViews($table)
     {
         $query = "SELECT name, sql FROM sqlite_master WHERE type='view' AND sql NOT NULL";
-        $views = $db->fetchAll($query);
+        $views = $this->conn->fetchAll($query);
 
         $result = [];
         foreach ($views as $row) {
