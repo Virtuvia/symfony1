407bc53412c2cfa13e9bfba180fa72e8834f4ca0
Matt Lehner <matt.lehner@coachmeplus.com>
2024-11-09 07:06:36 -0500

    add @deprecated to ArrayAccess and PropertyAccess traits

--- plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Record/ArrayAccessTrait.php
+++ plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Record/ArrayAccessTrait.php
@@ -4,21 +4,33 @@ declare(strict_types=1);
 
 trait Doctrine_Record_ArrayAccessTrait
 {
+    /**
+     * @deprecated
+     */
     final public function offsetExists(mixed $offset): bool
     {
         return $this->contains($offset);
     }
 
+    /**
+     * @deprecated use appropriate get* or {@see Doctrine_Record::get()}
+     */
     final public function offsetGet(mixed $offset): mixed
     {
         return $this->get($offset);
     }
 
+    /**
+     * @deprecated use appropriate set* or {@see Doctrine_Record::set()}
+     */
     final public function offsetSet(mixed $offset, mixed $value): void
     {
         $this->set($offset, $value);
     }
 
+    /**
+     * @deprecated
+     */
     final public function offsetUnset(mixed $offset): void
     {
         $this->remove($offset);
--- plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Record/PropertyAccessTrait.php
+++ plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Record/PropertyAccessTrait.php
@@ -4,21 +4,33 @@ declare(strict_types=1);
 
 trait Doctrine_Record_PropertyAccessTrait
 {
+    /**
+     * @deprecated use appropriate set* or {@see Doctrine_Record::set()}
+     */
     final public function __set(string $name, mixed $value): void
     {
         $this->set($name, $value);
     }
 
+    /**
+     * @deprecated use appropriate get* or {@see Doctrine_Record::get()}
+     */
     final public function __get(string $name): mixed
     {
         return $this->get($name);
     }
 
+    /**
+     * @deprecated
+     */
     final public function __isset(string $name): bool
     {
         return $this->contains($name);
     }
 
+    /**
+     * @deprecated
+     */
     final public function __unset(string $name): void
     {
         $this->remove($name);
