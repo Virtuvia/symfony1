6c639c2d87c29c0f9bf752a0d6d579a3e1b22644
Matt Lehner <matt.lehner@coachmeplus.com>
2024-11-14 09:36:09 -0500

    add more abstract methods to traits

--- plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Record/AbstractAccessorsTrait.php
+++ plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Record/AbstractAccessorsTrait.php
@@ -5,4 +5,14 @@ declare(strict_types=1);
 trait Doctrine_Record_AbstractAccessorsTrait
 {
     abstract public function get(string $fieldName, bool $load = true): mixed;
+
+    abstract protected function _get($fieldName, $load = true);
+
+    abstract protected function internalGetValue(string $fieldName): mixed;
+
+    abstract protected function internalGetData(string $fieldName, bool $load = true): mixed;
+
+    abstract protected function internalGetReference(string $fieldName, bool $load = true): Doctrine_Record|Doctrine_Collection|null;
+
+    abstract protected function internalGetReferenceOrNull(string $fieldName, bool $load = true): Doctrine_Record|Doctrine_Collection|null;
 }
--- plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Record/AbstractMutatorsTrait.php
+++ plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Record/AbstractMutatorsTrait.php
@@ -5,4 +5,6 @@ declare(strict_types=1);
 trait Doctrine_Record_AbstractMutatorsTrait
 {
     abstract public function set(string $fieldName, mixed $value, bool $load = true): static;
+
+    abstract protected function _set($fieldName, $value, $load = true);
 }
