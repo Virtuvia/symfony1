8088927bbdf353e73f1b3436dbbc5c5e44c6fe6e
Chris Castleman <chris.castleman@coachmeplus.com>
2025-11-18 06:22:19 -0800

    account for null args in sfValidatorError

--- lib/validator/sfValidatorError.class.php
+++ lib/validator/sfValidatorError.class.php
@@ -99,7 +99,7 @@ class sfValidatorError extends Exception
                 continue;
             }
 
-            $arguments["%$key%"] = htmlspecialchars($value, ENT_QUOTES, sfValidatorBase::getCharset());
+            $arguments["%$key%"] = $value === null ? '' : htmlspecialchars($value, ENT_QUOTES, sfValidatorBase::getCharset());
         }
 
         return $arguments;
