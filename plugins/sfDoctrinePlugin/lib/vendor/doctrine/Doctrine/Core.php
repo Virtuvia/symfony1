214b0aea03083336f3de09de449d0e8b496f6b6c
Matt Lehner <matt.lehner@coachmeplus.com>
2026-05-13 16:32:22 -0400

    bump Doctrine1 in sfDoctrinePlugin to 1.2.4.28

--- plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Core.php
+++ plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Core.php
@@ -35,7 +35,7 @@ class Doctrine_Core
     /**
      * VERSION
      */
-    public const VERSION                   = '1.2.4.27';
+    public const VERSION                   = '1.2.4.28';
 
     /**
      * ERROR CONSTANTS
